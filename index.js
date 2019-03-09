b474fee5eebb6376375292da8d59a4e69d58775d
Victor <victorjuez.alu@gmail.com>
2019-03-09 17:59:59 +0100

    changes to deploy on heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const db = require('./queries')
 
