fcff7de6ae5728b2b3b6c8ee4ae95a88a1b8e604
Victor <victorjuez.alu@gmail.com>
2019-03-21 13:11:40 +0100

    API calls to all DB tables done

--- back-end/index.js
+++ back-end/index.js
@@ -30,6 +30,35 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
+// AUTHORS routes
+app.get('/authors', db.getAuthors)
+app.get('/authors/:id', db.getAuthorById)
+app.post('/authors', db.createAuthor)
+app.put('/authors/:id', db.updateAuthor)
+app.delete('/authors/:id', db.deleteAuthor)
+
+// BOOKS routes
+app.get('/books', db.getBooks)
+app.get('/books/:id', db.getBookById)
+app.post('/books', db.createBook)
+app.put('/books/:id', db.updateBook)
+app.delete('/books/:id', db.deleteBook)
+
+// SALES routes
+app.get('/sales', db.getSales)
+app.get('/sales/:id', db.getSaleById)
+app.post('/sales', db.createSale)
+app.put('/sales/:id', db.updateSale)
+app.delete('/sales/:id', db.deleteSale)
+
+// RATINGS routes
+app.get('/ratings', db.getRatings)
+app.get('/ratings/:id', db.getRatingById)
+app.post('/ratings', db.createRating)
+app.put('/ratings/:id', db.updateRating)
+app.delete('/ratings/:id', db.deleteRating)
+
+
 // console output when app is executed to know it is working.
 app.listen(port, () => {
     console.log(`App running on port ${port}..`)
