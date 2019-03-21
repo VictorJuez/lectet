69d541a0ec95ee7fce9f8b43deaff9aef2da0110
Victor <victorjuez.alu@gmail.com>
2019-03-21 18:41:44 +0100

    Few changes at frontend sample

--- static-files/main.js
+++ static-files/main.js
@@ -1,36 +1,50 @@
-
-function loadData(i) {
-
+$(document).ready(() => {
     var $body = $('body');
-    var $foundUser = $('#found-user');
-    var idUser = $('#id-user').val();
 
-    $foundUser.text('User with id: ' + idUser + '-> ');
-
-    var userURL = 'users/' + idUser;
-    if(i == 0) userURL = 'users/';
-
-    $.ajax({
-        url: userURL,
-        type: 'GET',
-        dataType: "json",
-        success: function( response ) {
-            console.log(response);
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            console.log(errorThrown);
-         }
+    $('#single-user-btn').click(() => {
+        const idUser = $('#id-user').val();
+        var userURL = 'users/' + idUser;
+    
+        $.ajax({
+            url: userURL,
+            type: 'GET',
+            dataType: "json",
+            success: function( response ) {
+                console.log(response);
+                const user = response[0];
+                $('#found-user').text('User found:');
+                $body.append('<p>name:'+ user.name +' email:'+ user.email+'</p>');
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log(errorThrown);
+             }
+        });
     });
     
-    /*$.ajax({url: "http://localhost:3000/users/"+idUser, success: function(result){
-        //$("#div1").html(result);
-        console.log(result);
-    }});*/
-
-    return false;
-};
-
-$('#all-users-btn').click(loadData(0));
-
-$('#single-user-btn').click(loadData(1));
-
+    $('#all-users-btn').click(() => {
+        console.log("button clicked!");
+        var userURL = 'users/';
+    
+        $.ajax({
+            url: userURL,
+            type: 'GET',
+            dataType: "json",
+            success: function( response ) {
+                console.log(response);
+                for(var i=0; i<response.length; ++i){
+                    var user = response[i];
+                    $('#found-user').text('User found:');
+                    $body.append('<p>name:'+ user.name +' email:'+ user.email+'</p>');
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log(errorThrown);
+             }
+        });
+    });
+    // define a generic Ajax error handler:
+    // http://api.jquery.com/ajaxerror/
+    $(document).ajaxError(() => {
+      console.log("unknown ajax error!");
+    });
+  });
