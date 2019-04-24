ce1aba23957afc3d566226eba457f50149cf950a
Victor <victorjuez.alu@gmail.com>
2019-04-24 15:06:37 +0200

    swagger added

--- app/server.js
+++ app/server.js
@@ -9,6 +9,8 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express()
 const port = process.env.PORT || 3000 // When app executed on local machine, port = 3000, when on Heroku, port = process.env.PORT
@@ -25,6 +27,7 @@ app.use(
 
 // API routes
 app.use('/api',routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Start the server
 app.listen(port, () => {
