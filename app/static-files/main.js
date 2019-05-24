385f66f9cd70ad85659be376d3217d7a1321cba2
Julián <julianmangut@gmail.com>
2019-05-24 12:41:05 +0200

    Fix main page

--- app/static-files/main.js
+++ app/static-files/main.js
@@ -77,6 +77,26 @@ $(document).ready(() => {
         return id;
     }
 
+    (function(){
+        $('.carousel-showmanymoveone .item').each(function(){
+          var itemToClone = $(this);
+      
+          for (var i=1;i<4;i++) {
+            itemToClone = itemToClone.next();
+      
+            // wrap around if at end of item collection
+            if (!itemToClone.length) {
+              itemToClone = $(this).siblings(':first');
+            }
+      
+            // grab item, clone, add marker class, add to collection
+            itemToClone.children(':first-child').clone()
+              .addClass("cloneditem-"+(i))
+              .appendTo($(this));
+          }
+        });
+      }());
+
     /*  function ajaxGet(url, callback) {
           var req = new XMLHttpRequest();
           req.open("GET", url, true);
