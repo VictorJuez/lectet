7cf1755c40ca49655605271a79cf7cb68b23c476
Julián <julianmangut@gmail.com>
2019-05-26 13:02:13 +0200

    Sticky menu and fixed footer

--- app/static-files/main.js
+++ app/static-files/main.js
@@ -113,6 +113,18 @@ $(document).ready(() => {
     
 
 
+    $(document).ready(function(){
+        var altura = $('#menu-top').offset().top;
+        
+        $(window).on('scroll', function(){
+            if ( $(window).scrollTop() > altura ){
+                $('#menu-top').addClass('menu-fixed');
+            } else {
+                $('#menu-top').removeClass('menu-fixed');
+            }
+        });
+     
+    });
 
 
     
