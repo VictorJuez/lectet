c7ed0ba71850bfa4ec33c3ff22e86b3381ba40fa
Victor <victorjuez.alu@gmail.com>
2019-05-29 11:46:04 +0200

    Navbar loading user

--- app/static-files/menu.js
+++ app/static-files/menu.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     var altura = $('#menu-top').offset().top;
     
+    const userkey = JSON.parse(window.localStorage.getItem("lectet"));
+    if(userkey){
+        $user = $('#loggeduser');
+        $user.text(userkey.email);
+    }
+    else console.log("you are not logged in");
+    
     $(window).on('scroll', function(){
         if ( $(window).scrollTop() > altura ){
             $('#menu-top').addClass('menu-fixed');
