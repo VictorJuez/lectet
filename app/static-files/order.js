3f1db160da7b4a06cce97d40fc6b199764eb8b2a
Julián <julianmangut@gmail.com>
2019-06-11 17:05:20 +0200

    Changes on search page

--- app/static-files/order.js
+++ app/static-files/order.js
@@ -25,6 +25,7 @@ var newPrices = [];
 var id_books = [];
 var quantityNew = [];
 var length_cart = 0;
+var total;
 
 $(document).ready(() => {
   $.ajax({
@@ -50,7 +51,7 @@ $(document).ready(() => {
       var book_order_html = '';
       var price_order_html = '';
 
-      var total = 0;
+      total = 0;
 
       console.log(response.cart.books.length);
 
@@ -58,36 +59,41 @@ $(document).ready(() => {
 
       for (var x = 0; x < response.cart.books.length; x++) {
 
-        book_order_html = book_order_html + '<h4>' + response.cart.books[x].name + '</h4>';
+        book_order_html = book_order_html + '<h4 id="extract-name-book_' + x + '">' + response.cart.books[x].name + '</h4>';
 
-        price_order_html = price_order_html + '<h4 id="extract-book_' + x + '" class="price">' + response.cart.books[x].price * response.cart.books[x].cart_book.quantity + ' €</h4>';
+        price_order_html = price_order_html + '<h4 id="extract-price-book_' + x + '" class="price">' + response.cart.books[x].price * response.cart.books[x].cart_book.quantity + ' €</h4>';
 
         total = total + (response.cart.books[x].price * response.cart.books[x].cart_book.quantity);
 
         normalPrice.push(response.cart.books[x].price);
-        newPrices.push(response.cart.books[x].price);
+        newPrices.push(response.cart.books[x].price * response.cart.books[x].cart_book.quantity);
         id_books.push(response.cart.books[x].id);
         quantityNew.push(response.cart.books[x].cart_book.quantity);
 
-        book = book + '<div class="container">' + '<div class="row">' +
-          '<div class="col-sm-4 col-xs-5 product">' +
-          '<img src="./images/books/book_' + response.cart.books[x].id + '.jpg" class="img-responsive order-img" alt="Image">' +
-          '</div>' +
-          '<div class="col-sm-6 col-xs-3">' +
-          '<h3>' + response.cart.books[x].name + '</h3>' +
-          '<h6 class="description-book" id="description-book_' + x + '">' + response.cart.books[x].description + '</h6>' +
-          '</div>' +
-          '<div class="col-sm-2 col-xs-3">' +
-          '<h5 id="price-book_' + x + '">Price: ' + response.cart.books[x].price * response.cart.books[x].cart_book.quantity + ' €</h5>' +
-          '<select id="' + x + '" class="form-control" name="Quantity">' +
-          '<option value="1" selected>1</option>' +
-          '<option value="2">2</option>' +
-          '<option value="3">3</option>' +
-          '<option value="4">4</opion>' +
-          '<option value="5">5</option>' +
-          '</select>' +
-          '</div>' +
-          '</div>' +
+        book = book + 
+        '<div class="container book_' + x + '">' + 
+          '<div class="row">' +
+            '<div class="col-sm-4 col-xs-5 product">' +
+              '<img src="./images/books/book_' + response.cart.books[x].id + '.jpg" class="img-responsive order-img" alt="Image">' +
+            '</div>' +
+            '<div class="col-sm-6 col-xs-3">' +
+              '<h3>' + response.cart.books[x].name + '</h3>' +
+              '<h6 class="description-book" id="description-book_' + x + '">' + response.cart.books[x].description + '</h6>' +
+            '</div>' +
+            '<div class="col-sm-2 col-xs-3">' +
+              '<h5 id="price-book_' + x + '" >Price: ' + response.cart.books[x].price * response.cart.books[x].cart_book.quantity + ' €</h5>' +
+              
+              '<select id="' + x + '" class="form-control" name="Quantity">' +
+                '<option value="1" selected>1</option>' +
+                '<option value="2">2</option>' +
+                '<option value="3">3</option>' +
+                '<option value="4">4</opion>' +
+                '<option value="5">5</option>' +
+              '</select>' + 
+              '<button id="' + x + '" class="delete" onclick="deleteBook(this.id)"> X </button>' +
+              
+              '</div>' +
+            '</div>' +
           '</div>' + '<br/>' + '<br/>'
       }
 
@@ -118,41 +124,68 @@ $(document).ready(() => {
   var price = [];
   var extract = [];
 
-  var total = document.getElementById('total-price');
+  var totalElement = document.getElementById('total-price');
 
   for (var x = 0; x < length_cart; x++) {
     quantitySelected.push(document.getElementById(x));
     price.push(document.getElementById('price-book_' + x));
-    extract.push(document.getElementById('extract-book_' + x));
+    extract.push(document.getElementById('extract-price-book_' + x));
 
     quantitySelected[x].addEventListener('change',
     function () {
 
       console.log(this.id);
 
-      var totalPrice = 0;
-
       quantity = this.options[this.selectedIndex];
       quantityNew[this.id] = quantity.value;
       console.log(quantity.value + ': ' + quantity.text);
       newPrices[this.id] = normalPrice[this.id] * quantity.value;
 
+      total = 0;
+
       console.log("NO ESTOY TOCANDO NADA");
 
-      $(price[this.id]).text("Price: " + newPrices[this.id] + " €");
+      //$(price[this.id]).text("Price: " + newPrices[this.id] + " €");
       $(extract[this.id]).text(newPrices[this.id] + " €");
 
       for (var x = 0; x < newPrices.length; x++) {
-        totalPrice = totalPrice + newPrices[x];
+       
+        console.log("New Price: " + newPrices[x]);
+       
+        total = total + newPrices[x];
       }
 
-      $(total).text(totalPrice + " €");
+      $(totalElement).text(total + " €");
 
     });
   }
 })
 
+function deleteBook (x) {
+  console.log(x);
+  $(".book_" + x).css({
+    'display': 'none'
+  });
+  $("#extract-price-book_" + x).css({
+    'display': 'none'
+  });
+  $("#extract-name-book_" + x).css({
+    'display': 'none'
+  });
 
+  var totalElement = document.getElementById('total-price');
+
+  console.log(total);
+
+  console.log(newPrices[x]);
+
+  total = total - newPrices[x];
+
+  $(totalElement).text(total + " €");
+
+  id_books[x] = -1;
+  newPrices[x] = 0;
+}
 
 $(document).ready(() => {
   $.ajax({
@@ -208,20 +241,45 @@ function getRandomInt(min, max, list_id) {
 
 document.getElementById("checkout").onclick = function () {
   addToCart();
-  location.href='./shippment-info.html';
+  //location.href='./shippment-info.html';
 };
 
 function addToCart() {
   
+  var x = 0;
+
+  var isGood = false;
+  var isOnly = false;
+
   var new_data = "[";
 
-  new_data = new_data + '{' + '"book"' + ':' + id_books[0] + ',' + '"quantity"' + ':' + quantityNew[0] + '}'
 
-  for(var x = 1; x < id_books.length; x++) {
-    new_data = new_data + ',{' + '"book"' + ':' + id_books[x] + ',' + '"quantity"' + ':' + quantityNew[x] + '}'
+  while(x < id_books.length && isGood == false) {
+    if(id_books[x] != -1) {
+      new_data = new_data + '{' + '"book"' + ':' + id_books[x] + ',' + '"quantity"' + ':' + quantityNew[x] + '}'
+
+      isGood = true;
+
+      if((x + 1) == id_books.length){
+        isOnly = true;
+      }
+
+      x++;
+    }
+    else {
+      x++;
+    }
   }
-
+  
+  if(isOnly == false) {
+    for(x; x < id_books.length; x++) {
+      new_data = new_data + ',{' + '"book"' + ':' + id_books[x] + ',' + '"quantity"' + ':' + quantityNew[x] + '}'
+    }
+  }
+  
   new_data = new_data + "]";
+
+  console.log(new_data);
 
   $.ajax({
       url: 'https://lectet.herokuapp.com/api/cart/',
--- app/static-files/search.js
+++ app/static-files/search.js
@@ -7,43 +7,93 @@ $.urlParam = function (name) {
     }
 }
 
-
+var selectedUrl;
 
 $(document).ready(() => {
-    console.log("id=" + $.urlParam('id'));
-    console.log("Name = " + $.urlParam('name'));
 
-    console.log('https://lectet.herokuapp.com/api/books/' + $.urlParam('name') + '/' + $.urlParam('id'))
-    $.ajax({
-        type: 'GET',
-        //url: 'https://lectet.herokuapp.com/api/books/' + $.urlParam('name') + '/' + $.urlParam('id'),
-        url: 'https://lectet.herokuapp.com/api/books/' + $.urlParam('name') + '/3',
-        success: function (respond) {
-            console.log(respond);
+    if($.urlParam('name') != "authors") {
 
-            var search = "#books-search";
+        if ($.urlParam('name') == "books") {
 
-            var book_search = '';
-
-            for (var x = 0; x < 5; x++) {
-
-                book_search = book_search + '<div class="col-lg-4 col-sm-6 mb-4">' +
-
-                    '<div class="card" style="width: 18rem;">' +
-                    '<img src="./images/books/book_' + respond.books[x].id + '.jpg" class="fakeimg" alt="...">' +
-                    '<div class="card-body">' +
-                    '<h5 class="card-title">' + respond.books[x].name + '</h5>' +
-                    '<p class="card-text">' + respond.books[x].description + '</p>' +
-                    '<a href="./product.html?id=' + respond.books[x].id + '" class="btn btn-primary">Go somewhere</a>' +
-                    ' </div> ' +
-                    '</div>' +
-
-                    '</div>'
-            }
-
-            $(search).html(book_search);
-
+            console.log("Name = " + $.urlParam('name'));
+    
+            selectedUrl = 'https://lectet.herokuapp.com/api/books/'
+    
+    
+        } else {
+           
+            //url: 'https://lectet.herokuapp.com/api/books/' + $.urlParam('name') + '/' + $.urlParam('id'),
+    
+            selectedUrl = 'https://lectet.herokuapp.com/api/books/' + $.urlParam('name') + '/3';
+    
         }
-    });
 
+        $.ajax({
+            type: 'GET',
+            url: selectedUrl,
+            success: function (respond) {
+                console.log(respond);
+    
+                var search = "#books-search";
+    
+                var book_search = '';
+    
+                for (var x = 0; x < 5; x++) {
+    
+                    book_search = book_search + '<div class="col-lg-4 col-sm-6 mb-4 card-book">' +
+    
+                        '<div class="card book" style="width: 18rem;">' +
+                        '<img src="./images/books/book_' + respond.books[x].id + '.jpg" class="fakeimg" alt="...">' +
+                        '<div class="card-body">' +
+                        '<h4 class="card-title">' + respond.books[x].name + '</h4>' +
+                        '<a href="./author.html?id=' + respond.books[x].authors[0].id + '></a>' +
+                        '<h6 class="card-text">' + respond.books[x].authors[0].name + ' ' + respond.books[x].authors[0].lastName + '</h6>' +
+                        '<a href="./product.html?id=' + respond.books[x].id + '" class="btn btn-primary button-book">' + respond.books[x].price + ' €' + '</a>' +
+                        ' </div> ' +
+                        '</div>' +
+    
+                        '</div>'
+                }
+    
+                $(search).html(book_search);
+    
+            }
+        });
+
+    }
+
+    else {
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://lectet.herokuapp.com/api/authors/',
+            success: function (respond) {
+                console.log(respond);
+    
+                var search = "#books-search";
+    
+                var book_search = '';
+    
+                for (var x = 0; x < 4; x++) {
+    
+                    book_search = book_search + '<div class="col-lg-4 col-sm-6 mb-4">' +
+    
+                        '<div class="card author" style="width: 18rem;">' +
+                        '<img class="author-img" src="./images/authors/author_1.jpg" class="fakeimg" alt="...">' +
+                        '<div class="card-body">' +
+                        '<h4 class="card-title">' + respond[x].name + ' ' + respond[x].lastName + '</h4>' +
+                        '<a href="./product.html?id=' + respond[x].id + '" class="btn btn-primary button-book"> Go to profile </a>' +
+                        ' </div> ' +
+                        '</div>' +
+    
+                        '</div>'
+                }
+    
+                $(search).html(book_search);
+    
+            }
+        });
+
+
+    }
 });
