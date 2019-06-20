68308b211f273b740abff3a3e224c1581aeb7391
Victor <victorjuez.alu@gmail.com>
2019-06-20 12:52:37 +0200

    Register all errors controlled

--- app/public/assets/js/registration.js
+++ app/public/assets/js/registration.js
@@ -12,6 +12,7 @@ $(document).ready(() => {
             success: function (response) {
                 console.log("Im logged in! ");
                 console.log(response);
+                window.location.href = "../index.html";
             },
             error: function () {
                 console.log("Error while checking logging");
@@ -23,9 +24,28 @@ $(document).ready(() => {
     $button.click(function () {
         const $email = $('#email').val();
         const $password = $('#password').val();
+        const $confirmPassword = $('#confirm_password').val();
+        const $firstName = $('#first_name').val();
+        const $secondName = $('#second_name').val();
+        const $terms = $('#t_and_c');
+        console.log("name: " + $firstName);
+        console.log("surname: " + $secondName);
         console.log("email: " + $email);
         console.log("password: " + $password);
-
+        console.log("confirmPassword: " + $confirmPassword);
+        if($firstName == "" || $secondName == "" || $password == "" || $email == "" || $confirmPassword == ""){
+            throwError("Please fill all the camps");
+            return;
+        }
+        if(!passwordEquals($password, $confirmPassword)){
+            throwError("Passwords are not equals");
+            return;
+        }
+        if($terms.is(":checked")) console.log("terms and conditions: checked");
+        else {
+            throwError("You have to accept the terms and conditions");
+            return;
+        }
         var check = $.ajax({
             url: 'https://lectet.herokuapp.com/backend/user/signUp',
             type: 'POST',
@@ -43,13 +63,29 @@ $(document).ready(() => {
                 }));
                 window.location.href = "../index.html";
             },
-            error: function () {
-                console.log("No se ha podido obtener la información");
+            error: function (error) {
+                console.log(error);
+                if(error.status == 400) throwError(error.responseJSON.details[0].message);
+                else if(error.status == 403) throwError(error.responseJSON.error);
+                else throwError("Error ocurred");
             }
         });
         console.log(check);
     });
 });
+
+function passwordEquals(password, password2){
+    if(password == password2) return true;
+    return false;
+}
+
+function throwError(message){
+    $("#error").css({
+        'display': 'block'
+    });
+    $('#error_text').text(message);
+    return;
+}
 
 /*
 save data to localstorage:      window.localStorage.setItem(key, value);
